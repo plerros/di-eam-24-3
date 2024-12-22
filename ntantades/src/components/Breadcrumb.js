e486390d7ad9352d27ca67cb9602315a0c1a46c6
plerros <48536508+plerros@users.noreply.github.com>
2024-12-22 11:37:55 +0200

    breadcrumb padding

--- ntantades/src/components/Breadcrumb.js
+++ ntantades/src/components/Breadcrumb.js
@@ -1,5 +1,8 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Container from '@mui/material/Container';
 import Link from '@mui/material/Link';
 
 import ListItemButton from '@mui/material/ListItemButton';
@@ -55,24 +58,26 @@ export default function Breadcrumb() {
   const pathnames = location.pathname.split('/').filter((x) => x);
 
   return (
-    <Breadcrumbs aria-label="breadcrumb">
-      <LinkRouter underline="hover" color="inherit" to="/">
-        Αρχική
-      </LinkRouter>
-      {pathnames.map((value, index) => {
-        const last = index === pathnames.length - 1;
-        const to = `/${pathnames.slice(0, index + 1).join('/')}`;
+      <Container maxWidth="xl">
+          <Breadcrumbs aria-label="breadcrumb">
+            <LinkRouter underline="hover" color="inherit" to="/">
+              Αρχική
+            </LinkRouter>
+            {pathnames.map((value, index) => {
+              const last = index === pathnames.length - 1;
+              const to = `/${pathnames.slice(0, index + 1).join('/')}`;
 
-        return last ? (
-          <Typography key={to} sx={{ color: 'text.primary' }}>
-            {breadcrumbNameMap(to).text || []}
-          </Typography>
-        ) : (
-          <LinkRouter underline="hover" color="inherit" to={to} key={to}>
-            {breadcrumbNameMap(to).text}
-          </LinkRouter>
-        );
-      })}
-    </Breadcrumbs>
+              return last ? (
+                <Typography key={to} sx={{ color: 'text.primary' }}>
+                  {breadcrumbNameMap(to).text || []}
+                </Typography>
+              ) : (
+                <LinkRouter underline="hover" color="inherit" to={to} key={to}>
+                  {breadcrumbNameMap(to).text}
+                </LinkRouter>
+              );
+            })}
+          </Breadcrumbs>
+      </Container>
   );
 }
