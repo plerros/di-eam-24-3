07f8aa35c0b573d74a266f3d64968766592dec41
plerros <48536508+plerros@users.noreply.github.com>
2024-12-30 11:15:13 +0200

    login
    - login page with error handling
    - input components to handle text & password

--- ntantades/src/pages/Login.js
+++ ntantades/src/pages/Login.js
@@ -1,7 +1,106 @@
-export default function Login({handleRoleNanny, handleRoleFamily, handleRoleNone}) {
+import * as React from 'react';
+import { Navigate } from "react-router-dom";
+import Box from '@mui/material/Box';
+import { Button } from '@mui/material';
+
+import getUser from '../components/getUser'
+import InputText from '../components/InputText';
+import InputPassword from '../components/InputPassword';
+
+import Database from "../data.json";
+
+export default function Login({uid, handleUID}) {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [submit, setSubmit] = React.useState(false)
+  const [issue, setIssue] = React.useState({error: false, help:""});
+  
+  const handleSubmit = () => {
+    setSubmit(true);
+  };
+
+  // If the input changes, reset the error.
+  React.useEffect(() => {
+    setIssue({error: false, help:""});
+  }, [email, password]);
+
+  React.useEffect(() => {
+    if (submit) {
+      // Find the user based on credentials
+      var user = Database.users.filter(item => (item.email === email) && (item.password === password))[0];
+      if (user === null || typeof user === 'undefined')
+        user = Database.users.filter(item => item.userID === 0)[0]
+    
+      if (user.userID !== 0) {
+        handleUID(user.userID)
+      }
+      else {
+        // if the UID is the special user 0, throw an appropriate error
+        if (email === "" && password === "")
+          setIssue({error:true, help:"Υποχρεωτικό"})
+        else
+          setIssue({error:true, help:"λάθος e-mail / κωδικός"})
+      }
+      setSubmit(false)
+    }
+  }, [email, password, submit, handleUID]);
+  
+  /* Already logged in */
+  if (uid !== 0) {
+    const user = getUser(uid);
+    if (user.role === "Nanny") {
+      return (
+        <Navigate to="/nanny" />
+      );
+    }
+    if (user.role === "Family") {
+      return (
+        <Navigate to="/family" />
+      );
+    }
+  }
+
   return (
-    <div>
-      <h1>This is the Login page</h1>
-    </div>
+    <Box
+      component="form"
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column',
+        '& > :not(style)': { m: 1, width: '25ch'} 
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <item>
+        <h1>Είσοδος</h1>
+      </item>
+      <InputText
+        label={"e-mail"}
+        required={true}
+        value={email}
+        setValue={setEmail}
+        issue={issue}
+      />
+      <InputPassword
+        value={password}
+        setValue={setPassword}
+        issue={issue}/>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center'
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <Button
+          variant="contained"
+          onClick={handleSubmit}
+        >
+          ΕΙΣΟΔΟΣ
+        </Button>
+      </Box>
+    </Box>
   );
 }
